5e4f765822c83617f683902793b88f7361456c91
khanfaizan123 <88190052+khanfaizan123@users.noreply.github.com>
2023-03-28 13:31:56 -0400

    Add files via upload

---
+++ src/features/bucketSlice.js
@@ -0,0 +1,128 @@
+import { createSlice, nanoid, } from '@reduxjs/toolkit';
+// import  cardData from "../app/model.json";
+
+
+const initialState = {
+    buckets: [
+        {
+            id: nanoid(),
+            name: "Environment",
+            cards: [
+                {
+                    id: nanoid(),
+                    title: "card1",
+                    link: "https://www.youtube.com/embed/QQYgCxu988s"
+                }
+            ]
+        },
+        {
+            id: nanoid(),
+            name: "Sports",
+            cards: [
+                {
+                    id: nanoid(),
+                    title: "card1",
+                    link: "https://www.youtube.com/embed/pxZ2RisaAp8"
+                }
+            ]
+        },
+        {
+            id: nanoid(),
+            name: "Entertainment",
+            cards: [
+                {
+                    id: nanoid(),
+                    title: "card1",
+                    link: "https://www.youtube.com/embed/MGPQGP2_jCA"
+                }
+            ]
+        },
+        {
+            id: nanoid(),
+            name: "Education",
+            cards: [
+                {
+                    id: nanoid(),
+                    title: "card1",
+                    link: "https://www.youtube.com/embed/Ed3JT-gd6HE"
+                }
+            ]
+        },
+        
+    ],
+}
+
+export const bucketSlice = createSlice({
+    name: "buckets",
+    initialState,
+    reducers: {
+        editBucketName: {
+            reducer(state, action) {
+                const { editedName, id } = action.payload
+                const foundBucket = state.buckets.find(bucket => bucket.id === id)
+                if (foundBucket) {
+                    foundBucket.name = editedName
+                }
+            }
+        },
+        deleteBucket: {
+            reducer(state, action) {
+                const { index } = action.payload
+                state.buckets.splice(index, 1);
+            }
+        },
+        addBucket: {
+            reducer(state, action) {
+                state.buckets.unshift({
+                    name: "new bucket",
+                    id: nanoid(),
+                    cards: [],
+                    initialEdit: true,
+                })
+            }
+        },
+        addCard: {
+            reducer(state, action) {
+                const { bucketIndex, title, link } = action.payload;
+                const newCard = {
+                    id: nanoid(),
+                    title,
+                    link,
+                }
+
+                const foundBucket = state.buckets.find((bucket, index) => index === bucketIndex)
+                foundBucket.cards.unshift(newCard)
+            }
+        },
+        updateCard: {
+            reducer(state, action) {
+                const { title, link, bucketIndex, cardIndex } = action.payload
+                const foundBucket = state.buckets.find((bucket, index) => index === bucketIndex)
+                const foundCard = foundBucket.cards[cardIndex]
+                foundCard.title = title;
+                foundCard.link = link;
+            }
+        },
+        deleteCard: {
+            reducer(state, action) {
+                const { bucketIndex, cardIndex } = action.payload
+                console.log(action.payload, "deelele");
+                state.buckets[bucketIndex].cards.splice(cardIndex, 1);
+            }
+        },
+        toggleInitialEditValue: {
+            reducer(state, action) {
+                const { index } = action.payload
+                const val = state.buckets[index].initialEdit
+                if (val !== undefined) {
+                    state.buckets[index].initialEdit = false;
+                }
+            }
+        }
+    }
+})
+
+export const allBuckets = (state) => state.buckets.buckets
+export const { editBucketName, addCard, updateCard, deleteBucket, addBucket, deleteCard, toggleInitialEditValue } = bucketSlice.actions
+
+export default bucketSlice.reducer
